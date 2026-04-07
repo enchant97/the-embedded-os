fc3c0d8a47079c6d0b388a3e168352e2c13c1d60
Leo Spratt <enchant97@users.noreply.github.com>
2026-04-07 20:19:11 +0100

    remove macro as it's not used

--- libsys/src/lib.rs
+++ libsys/src/lib.rs
@@ -8,17 +8,3 @@ pub mod process;
 
 pub use kernel_abi::KernelAbi; // remove when proc-macro is implemented for _start
 pub use process::ExitCode;
-
-#[macro_export]
-macro_rules! main_entrypoint {
-    ($main:path) => {
-        const _: fn() -> $crate::process::ExitCode = $main;
-
-        #[unsafe(no_mangle)]
-        #[unsafe(link_section = ".text._start")]
-        pub extern "C" fn _start(abi: *const ::kernel_abi::KernelAbi) -> ! {
-            $crate::core::sys_init(abi);
-            $crate::process::exit($main())
-        }
-    };
-}
