04f7019710babd987d362a3004b647a5c225aeaf
Leo Spratt <enchant97@users.noreply.github.com>
2026-04-10 15:10:43 +0100

    add "Hello World" output to shell

--- libsys/src/lib.rs
+++ libsys/src/lib.rs
@@ -8,3 +8,24 @@ pub mod process;
 
 pub use kernel_abi::KernelAbi; // remove when proc-macro is implemented for _start
 pub use process::ExitCode;
+
+/// Re-export used parts of nostd.
+pub mod nostd {
+    pub mod io {
+        pub use nostd::io::{Read, Write};
+    }
+}
+
+/// Custom panic handler for capturing Rust panics.
+///
+/// Will send relevant exit code to kernel.
+#[cfg(target_os = "none")]
+pub mod panic_system {
+    use crate::{ExitCode, process::exit};
+    use core::panic::PanicInfo;
+
+    #[panic_handler]
+    fn panic(_: &PanicInfo) -> ! {
+        exit(ExitCode::GeneralError);
+    }
+}
--- shell/src/main.rs
+++ shell/src/main.rs
@@ -1,14 +1,18 @@
 #![no_std]
 #![no_main]
 
-use libsys::ExitCode;
+use libsys::{ExitCode, nostd::io::Write};
 
 #[unsafe(no_mangle)]
 #[unsafe(link_section = ".text._start")]
 pub extern "C" fn _start(abi: *const libsys::KernelAbi) -> ExitCode {
     libsys::core::sys_init(abi);
     libsys::display::display().lock(|d| {
-        d.set_display_mode(libsys::display::DisplayMode::Text);
+        d.set_display_mode(libsys::display::DisplayMode::Text)
+            .expect("failed to set mode");
+        d.write_all(b"Hello World!")
+            .expect("fail to write to display");
+        d.flush().expect("fail to flush display buffer");
     });
     ExitCode::Ok
 }
